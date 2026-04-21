28570c1641735076be885d4bcf3fba76d91be704
Jared Holgate <jaredholgate@microsoft.com>
2026-04-21 12:31:20 +0100

    examples: drop unused location var from retrievable_secret_existing_keyvault

--- examples/retrievable_secret_existing_keyvault/variables.tf
+++ examples/retrievable_secret_existing_keyvault/variables.tf
@@ -3,11 +3,6 @@ variable "key_vault_id" {
   description = "The resource ID of an existing Key Vault to write the retrievable secret into. Provided by `pre.sh` via `terraform.tfvars`."
 }
 
-variable "location" {
-  type        = string
-  description = "The Azure region where the existing Key Vault resides. Provided by `pre.sh` via `terraform.tfvars`."
-}
-
 variable "enable_telemetry" {
   type        = bool
   default     = false
